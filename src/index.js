a7506a2fde0f0fc021724ae53859b302524cf952
TravisBarryDick <travis.barry.dick@gmail.com>
2020-02-19 16:41:46 -0500

    Added higher degree taylor approximations and controls for the number of decimal points

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import {Array2D} from './Array2D.js';
 import {DifferenceTable} from './DifferenceTable.js';
 
-// Returns the degree 3 Taylor expansion of log(x) at x = a
-function log_taylor_series(a) {
-  const c0 = Math.log(a);
-  const c1 = 1/a;
-  const c2 = -1 / (a*a);
-  const c3 = 2 / (a*a*a);
-  return x => c0 + c1*(x - a) + c2/2*Math.pow(x-a, 2) + c3/6*Math.pow(x-a,3);
+// Returns a degree p taylor series approximation to log(x) at x = a
+function log_taylor_series(a, p) {
+  return function(x) {
+    let value = Math.log(a);
+    let sign = 1;
+    for (let i = 1; i <= p; i++) {
+      value += sign * Math.pow(x - a, i) / (i * Math.pow(a, i))
+      sign *= -1;
+    }
+    return value;
+  }
 }
 
 function getValue(name) {
@@ -25,6 +29,8 @@ window.generate = function() {
   // Pull the parameters for the tables out of the HTML form
   const start = getValue("start");
   const dx = getValue("increment");
+  const degree = getValue("degree");
+  const precision = getValue("precision");
   const rowsPerTable = getValue("rows");
   const numTables = getValue("numTables");
 
@@ -33,7 +39,7 @@ window.generate = function() {
     const tableStart = start + dx*rowsPerTable*i;
     const tableEnd = tableStart + dx*rowsPerTable;
     const midpoint = (tableStart + tableEnd) / 2;
-    const table = new DifferenceTable(log_taylor_series(midpoint), tableStart, dx, rowsPerTable, 4, 2, 4);
+    const table = new DifferenceTable(log_taylor_series(midpoint, degree), tableStart, dx, rowsPerTable, degree+1, 2, precision);
 
     filled.appendChild(table.create_table(true));
     unfilled.appendChild(table.create_table(false));
